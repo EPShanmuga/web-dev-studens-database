6ec15180bf92a3592d625e7792c4f4205b76bb55
EPShanmuga <shanmugaep8@gmail.com>
2016-06-10 21:07:51 +0530

    these files display the table contents based on some column.select_name.php displays the list in the order of name.select_roll.php displays the list in the order of roll number.select_dept.php displays list of students ina particular dept.

---
+++ select_roll.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+ALL LIST
+</title>
+</head>
+<body style="background-color:orange">
+<h style="background-color:black;color:orange;font-size:20px;">LIST ACCORDING TO ROLL NUMBER</h>
+<p style="font-size:20px;">
+<?php
+// Create connection
+$conn = new mysqli("localhost", "root", "","StudDB");
+// Check connection
+$sql = "SELECT Name,Roll,Dept,Email,Address,About FROM StudReg ORDER BY Roll";
+$result = $conn->query($sql);
+$var=1;
+if ($result->num_rows > 0)
+ 
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+echo "<br>".$var."<br>";
+
+echo "<b>"." Name: "."</b>" . $row["Name"]."<br>";
+echo "<b>"." Roll Number: "."</b>" . $row["Roll"]."<br>";
+echo "<b>"." Department: "."</b>".$row["Dept"]."<br>";
+echo "<b>"." Email Address: "."</b>".$row["Email"]."<br>";
+echo "<b>"." Address: "."</b>".$row["Address"]."<br>";
+echo "<b>"." About Me: "."</b>".$row["About"]."<br>";
+$var++;
+}
+ else 
+    echo "No results";
+
+$conn->close();
+?>
+</p>
+</body>
+</html>
